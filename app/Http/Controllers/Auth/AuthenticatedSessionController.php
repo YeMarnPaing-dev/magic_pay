4cfc78c1ddbbefeac008e4f1241f1c314cc695c2
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-08-04 19:17:56 +0630

    second

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+         dd('Hi ! This is Testing');
     }
 
     /**
