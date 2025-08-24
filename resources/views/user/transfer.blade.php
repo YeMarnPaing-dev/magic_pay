3b4c37dae53cde771156012a3b8ac13b888a1f8f
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-08-24 10:16:58 +0630

    hi testing

--- resources/views/user/transfer.blade.php
+++ resources/views/user/transfer.blade.php
@@ -16,15 +16,24 @@
             </div>
              <div class="form-group">
                 <label for="">To</label>
-                <input type="text" name="to_phone" class="form-control">
+                <input type="text" name="to_phone" class="form-control @error('to_phone')  is-invalid @enderror">
+                @error('to_phone')
+                <span class="invalid-feedback">{{$message}}</span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="">Amount</label>
-                <input type="number" name="amount" class="form-control">
+                <input type="number" name="amount" class="form-control @error('amount')  is-invalid @enderror">
+                @error('amounts')
+                <span class="invalid-feedback">{{$message}}</span>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="">Description</label>
-               <textarea name="description" class="form-control" id="" ></textarea>
+               <textarea name="description" class="form-control @error('description')  is-invalid @enderror" id="" ></textarea>
+            @error('description')
+                <span class="invalid-feedback">{{$message}}</span>
+                @enderror
             </div>
 
             <button type="submit" style="background: #5842E3; color:#fff;" class="btn btn-block mt-3">Continue</button>
