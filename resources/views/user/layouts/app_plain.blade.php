421b08784d123bc439abac594130fb9f51b6c8e5
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-09-03 06:02:18 +0630

    plain

--- resources/views/user/layouts/app_plain.blade.php
+++ resources/views/user/layouts/app_plain.blade.php
@@ -168,9 +168,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 {{-- modal  --}}
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> --}}
 <script>
     $(document).ready(function() {
         let token = document.head.querySelector('meta[name="csrf-token"]');
